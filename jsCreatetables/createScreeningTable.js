27a61a6e60fc9ac105f1628158c9e436f6cd74d5
oliverbskau <oliverbskau@gmail.com>
2021-10-13 13:06:11 +0200

    java js files

--- jsCreatetables/createScreeningTable.js
+++ jsCreatetables/createScreeningTable.js
@@ -2,24 +2,25 @@
 
 const table = document.getElementById("screeningsTable");
 
-function addRows(Screening){
-let rowCount = table.rows.length;
-let row = table.insertRow(rowCount);
-row.id = screening.screening;
+function addRows(Screening) {
+    let rowCount = table.rows.length;
+    let row = table.insertRow(rowCount);
+    row.id = screening.screening;
 
-let cellID = row.insertCell(0);
-cellID.innerHTML = screening.screeningID;
+    let cellID = row.insertCell(0);
+    cellID.innerHTML = screening.let
+    cellTimeStart = row.insertCell(1);
+    cellTime.innerHTML = screening.startTime;
 
-
-
-
-
-
-
-
-
-
-
+    let cellTimeEnd = row.insertCell(2);
+    cellTimeEnd.innerHTML = screening.endTime;
 
 
 }
+
+
+
+
+
+
+
