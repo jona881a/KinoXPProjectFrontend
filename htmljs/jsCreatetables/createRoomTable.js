b687c01969e18152421b3addb79378d0138355fc
khade <khad0122@stud.kea.dk>
2021-10-14 14:20:09 +0200

    logo

--- htmljs/jsCreatetables/createRoomTable.js
+++ htmljs/jsCreatetables/createRoomTable.js
@@ -1,1 +1,34 @@
-out("jeg er i create kommune tabel");
+out("jeg er i create room tabel");
+
+const table = document.getElementById("room_tabel");
+
+function addRow(room) {
+    let rowCount = table.rows.length;
+    let row = table.insertRow(rowCount);
+    row.id = room.room;
+
+    let cell1 = row.insertCell(0);
+    cell1.innerHTML = room.roomID;
+
+    //let cell2 = row.insertCell(1);
+    //cell2.innerHTML = room.room;
+    let cell2 = row.insertCell(1);
+    cell2.innerHTML = room.row;
+
+    let cell3 = row.insertCell(2);
+    cell3.innerHTML = room.seats;
+
+}
+
+function createTableFromMap(btn) {
+    out("create table");
+    table.innerHTML = "";
+    for (const roomKey of roomMap.keys()) {
+        const room1 = roomMap.get(roomKey);
+        addRow(room1);
+    }
+}
+
+const pbCreateTable = document.querySelector(".pbCreateTable");
+pbCreateTable.addEventListener("click", createTableFromMap);
+
